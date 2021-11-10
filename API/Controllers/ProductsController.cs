b60f06093f008b9c6f2dc82f6e1f8d779cdb0cd7
Ahsan khan <ahsankhnn@outlook.com>
2021-11-10 23:09:44 +0500

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //we are using 'api/' because its conventional
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController (StoreContext context)// created construcor and injected StoreContext Database and we can use it. Its dependency injection and its very easy to use Database in our code by implementing DI
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+        [HttpGet("{id}")] //we have made a route parameter here ("{id}")
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
